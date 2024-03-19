da5982d4e93fac5bc882f07c363c7054a6ba45b4
MNWake <theopkoester@icloud.com>
2024-03-19 11:32:05 -0400

    Initial Commit

---
+++ Cabled/CabledApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabledApp.swift
+//  Cabled
+//
+//  Created by Theo Koester on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CabledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
